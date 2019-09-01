65192b249eb75b579ba37edf9cd5eb370fea14cb
Carlos Rhedney <twisterpsa@hotmail.com>
2019-09-01 13:51:56 -0300

    ADD RequisicoesAssincronas

---
+++ Requisicoes assincronas com JQuery/pagina_3_conteudo.php
@@ -0,0 +1,21 @@
+<div class="panel panel-default">
+	<div class="panel-body">
+		<h1 class="h">Requisições Assincronas com JQuery</h1>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+			Duis quis iaculis sem. Sed fermentum sodales neque, vel porta sapien posuere vestibulum.
+			Phasellus elementum ac nulla vel tempor. Donec nisi felis, mattis eget nisl eu, euismod eleifend mi.
+			Curabitur egestas blandit metus, nec dapibus purus sollicitudin eu. Pellentesque sed tincidunt felis.
+			Nulla eget molestie metus. Sed placerat ante id turpis rutrum, id rutrum purus sollicitudin.
+			Aenean non urna id orci vulputate sollicitudin. Etiam a est urna. Maecenas imperdiet tincidunt mollis.
+			Praesent vel ullamcorper leo. Donec ornare, velit id fringilla accumsan, neque purus lobortis lacus, id commodo
+			nunc magna vel sapien. Ut et convallis enim. Sed at mi vitae purus faucibus fermentum a et quam.
+			Vestibulum consectetur aliquet lacus et convallis.
+			Sed faucibus nisi libero, eu semper magna tincidunt et. Sed ut hendrerit odio. Nunc auctor nec dui sed molestie.
+			Nam sagittis feugiat libero, eu facilisis orci facilisis ac. Suspendisse dictum purus in ullamcorper volutpat.
+			Etiam eu placerat nisl. In volutpat sodales massa, in dapibus massa scelerisque vel. Praesent in augue metus.
+			Nam laoreet faucibus velit, in luctus mi. Ut est risus, blandit vel rutrum a, aliquam quis quam.
+			Ut vel felis nec elit pellentesque maximus ut et lectus. Nulla vehicula arcu ut tortor porta, ut euismod sapien egestas.
+			Sed egestas sem vitae risus ultrices, non mattis magna dignissim.
+	</div>
+</div>
---
+++ Requisicoes assincronas com JQuery/pagina_4_conteudo.php
@@ -0,0 +1,14 @@
+<div class="panel panel-default">
+	<div class="panel-body">
+		<h1 class="h">Requisições Assincronas com JQuery</h1>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+			Duis quis iaculis sem. Sed fermentum sodales neque, vel porta sapien posuere vestibulum.
+			Phasellus elementum ac nulla vel tempor. Donec nisi felis, mattis eget nisl eu, euismod eleifend mi.
+			Curabitur egestas blandit metus, nec dapibus purus sollicitudin eu. Pellentesque sed tincidunt felis.
+			Nulla eget molestie metus. Sed placerat ante id turpis rutrum, id rutrum purus sollicitudin.
+			Aenean non urna id orci vulputate sollicitudin. Etiam a est urna. Maecenas imperdiet tincidunt mollis.
+			Praesent vel ullamcorper leo.
+		</p>
+	</div>
+</div>
